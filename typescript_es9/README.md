f63bef1796c41dd25c9399f1483f9848352fd95f
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-21 10:20:21 +0200

    Add files via upload

---
+++ typescript_es9/README.md
@@ -0,0 +1,3 @@
+# 09 - ARRAYS: Multi-dimensional Arrays
+
+Please provide type annotations for all the arrays provided in the // Multidimensional arrays: section.
---
+++ typescript_es9/script.js
@@ -0,0 +1,37 @@
+// Arrays:
+// let bestNumbers: number[] = [7, 77, 4];
+// let bestLunches: string[] = ["chicken soup", "non-chicken soup"];
+// let bestBreakfasts: string[] = [
+//   "fasting",
+//   "oatmeal",
+//   "tamago kake gohan",
+//   "any kind of soup",
+// ];
+// let bestBooleans: boolean[] = [true, false];
+// // Multidimensional Arrays:
+// let bestMealPlan = [
+//   bestLunches,
+//   bestBreakfasts,
+//   ["baked potato", "mashed potato"],
+// ];
+// let bestBooleansTwice = [bestBooleans, bestBooleans];
+// let numbersMulti = [
+//   [[1], [2, 3]],
+//   [[7], bestNumbers],
+// ];
+// Arrays:
+var bestNumbers = [7, 77, 4];
+var bestLunches = ['chicken soup', 'non-chicken soup'];
+var bestBreakfasts = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+var bestBooleans = [true, false];
+// Multidimensional Arrays:
+var bestMealPlan = [
+    bestLunches,
+    bestBreakfasts,
+    ["baked potato", "mashed potato"],
+];
+var bestBooleansTwice = [bestBooleans, bestBooleans];
+var numbersMulti = [
+    [[1], [2, 3]],
+    [[7], bestNumbers],
+];
