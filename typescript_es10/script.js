8e314a17f721cb7a7371124140ce48903e8d6fcb
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-21 10:39:25 +0200

    Add files via upload

---
+++ typescript_es10/script.js
@@ -0,0 +1,11 @@
+// let favoriteCoordinates = [40, 43.2, "N", 73, 59.8, "W"];
+var favoriteCoordinates = [
+    40,
+    43.2,
+    "N",
+    73,
+    59.8,
+    "W",
+];
+favoriteCoordinates = [17, 45, "N", 142, 30, "E"];
+favoriteCoordinates[6] = -6.825;
---
+++ typescript_es10/script.ts
@@ -0,0 +1,14 @@
+// let favoriteCoordinates = [40, 43.2, "N", 73, 59.8, "W"];
+
+let favoriteCoordinates: [number, number, string, number, number, string] = [
+  40,
+  43.2,
+  "N",
+  73,
+  59.8,
+  "W",
+];
+
+favoriteCoordinates = [17, 45, "N", 142, 30, "E"];
+
+favoriteCoordinates[6] = -6.825;
