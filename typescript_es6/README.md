acec20aeec5387c64d724d39799f785979b3184e
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-21 07:34:13 +0200

    Add files via upload

---
+++ typescript_es6/README.md
@@ -0,0 +1,4 @@
+# 06 - FUNCTIONS: Void Return Type
+
+* Use TypeScript to check this fruit salad code for type errors.
+* It looks like the type annotation for the return type of makeFruitSalad() isn’t quite right. Please correct this!
---
+++ typescript_es6/script.js
@@ -0,0 +1,10 @@
+// function makeFruitSalad(fruit1: string, fruit2: string): string {
+//   let salad = fruit1 + fruit2 + fruit2 + fruit1 + fruit2 + fruit1 + fruit1;
+//   console.log(salad);
+// }
+// makeFruitSalad("banana", "pineapple");
+function makeFruitSalad(fruit1, fruit2) {
+    var salad = fruit1 + fruit2 + fruit2 + fruit1 + fruit2 + fruit1 + fruit1;
+    console.log(salad);
+}
+makeFruitSalad("banana", "pineapple");
