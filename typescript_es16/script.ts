764de69cdb14f47063fc1e9a08d604a1a53471cc
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-22 13:50:17 +0200

    Add files via upload

--- typescript_es16/script.ts
+++ typescript_es16/script.ts
@@ -1,3 +1,27 @@
+// function sayHappyBirthdayWithObject(personObject){
+//   let output ='';
+//   output += 'Happy Birthday '
+//          + personObject.name + '! ';
+//   output += 'You are now ' 
+//          + personObject.age + ' years old! ';
+//   output += 'Your birthday wish was to receive ' 
+//          + personObject.giftWish 
+//          + '. And guess what? You will ';
+//   if (!personObject.success){
+//     output += 'not ';
+//   }
+//   output += 'receive it! \n';
+//   console.log(output);
+// }
+
+// let birthdayBabies = [
+//   {name: 'Liam', age: 0, giftWish: 'karate skills', success: false}, 
+//   {name: 'Olivia', age: 0, giftWish: 'a bright future', success:true}, 
+//   {name: 'Ava', age: 0, giftWish: '$0.25', success:true}
+// ]; 
+
+// birthdayBabies.forEach(sayHappyBirthdayWithObject);
+
 function sayHappyBirthdayWithObject(personObject: {
   name: string;
   age: number;
