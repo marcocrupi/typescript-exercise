3c3861a96ae1d33e9557c016dfcc0132f3186902
Marco Crupi <84478360+marcocrupi@users.noreply.github.com>
2022-09-22 13:46:49 +0200

    Add files via upload

---
+++ typescript_es16/script.ts
@@ -0,0 +1,26 @@
+let petOnSale = "chinchilla";
+let ordersArray = [
+  ["rat", 2],
+  ["chinchilla", 1],
+  ["hamster", 2],
+  ["chinchilla", 50],
+];
+
+// Write your code below:
+enum Pet {
+  Hamster = "HAMSTER",
+  Rat = "RAT",
+  Chinchilla = "CHINCHILLA",
+  Tarantula = "TARANTULA",
+}
+
+const petOnSaleTS: Pet = Pet.Chinchilla;
+
+const ordersArrayTS: [Pet, number][] = [
+  [Pet.Rat, 2],
+  [Pet.Chinchilla, 1],
+  [Pet.Hamster, 2],
+  [Pet.Chinchilla, 50],
+];
+
+ordersArrayTS.push(["HAMSTER", 1]);
